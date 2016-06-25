fa6512336b39b1b1643d3f6191cb26a4193c1186
kang <kang@kang-PC>
2016-06-25 16:27:59 +0900

    111

--- hack/hello-world-server.js
+++ hack/hello-world-server.js
@@ -1,7 +1,7 @@
 var serialport = require('serialport') ; 
 var SerialPort = serialport.SerialPort; 
 
-var serial = new SerialPort('/dev/ttyUSB0', { 
+var serial = new SerialPort('/dev/ttyAMA0', { 
 	baudrate : 9600,
 	parser: serialport.parsers.readline("\n")
 }) ; 
