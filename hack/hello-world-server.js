529edd8e0bacbec6c58e519a33c3b805c8dba430
kang <kang@kang-PC>
2016-06-25 18:50:16 +0900

    111

--- hack/hello-world-server.js
+++ hack/hello-world-server.js
@@ -4,7 +4,7 @@
    var img_cctv="";
    var helmet = "";
    var SerialPort = require("serialport").SerialPort;
-   var serialPort = new SerialPort("/dev/ttyAMA0", {
+   var serialPort = new SerialPort("/dev/ttyACM0", {
       baudrate: 9600
 
     });
