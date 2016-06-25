63b1856243dc0ea696cc52fb03147b27d01d9b9f
kang <kang@kang-PC>
2016-06-25 17:01:39 +0900

    11

--- hack/hello-world-server.js
+++ hack/hello-world-server.js
@@ -1,5 +1,9 @@
+   var request = require("request");
+   
+
+   var status = "";
    var SerialPort = require("serialport").SerialPort;
-    var serialPort = new SerialPort("/dev/ttyACM0", {
+   var serialPort = new SerialPort("/dev/ttyACM0", {
       baudrate: 9600
     });
     
@@ -7,7 +11,36 @@
       console.log('open');
       
       serialPort.on('data', function(data) {
-        console.log('data received: ' + data);
+    	  
+    		  /*
+    	  request({
+			  uri: "http://52.79.138.81/saint/worker/update",
+			  method: "POST",
+			  form: {
+				  id: "Bob",
+				  status: "Bob",
+				  img_cctv: "Bob",
+				  helmet: "Bob",
+			  }
+			}, function(error, response, body) {
+				
+			});*/
+    	  
+    	  if( status == "+panic" ) {
+    		  console.log('data received: ' + status);  
+    		  status = "";
+    	  } else {
+    		  status += data;
+    	  }
+
+    	  if( status == "+fall" ) {
+    		  console.log('data received: ' + status);  
+    		  status = "";
+    	  } else {
+    		  status += data;
+    	  }
+
+    		  
       });
       
       serialPort.write(new Buffer('4','ascii'), function(err, results) {
