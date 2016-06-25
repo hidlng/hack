ac9088c9f298d16b27d8fc6c8f13c2de1394d32d
kang <kang@kang-PC>
2016-06-26 06:36:55 +0900

    11

--- hack/hello-world-server.js
+++ hack/hello-world-server.js
@@ -1,74 +1,73 @@
    var request = require("request");
-   var status = "";
-   var id = "";
-   var img_cctv="";
-   var helmet = "";
+   var express = require('express');
+   var app = express();
+   var fs = require('fs');
+
+   var exec = require('child_process').exec;
+   var cmd = 'fswebcam /home/pi/photo/now.jpg';
    var SerialPort = require("serialport").SerialPort;
- /*
-   var http = require('http');
-   var options = {
-     host: '192.168.43.26',
-     path: '/',
-     port: '8080',
-     method: 'POST'
-   };*/
-
-
+   var state = "0";
    
    var serialPort = new SerialPort("/dev/ttyS0", {
       baudrate: 115200
     });
-    
+
+   app.get('/take', function (req, res) {
+     exec(cmd, function(error, stdout, stderr) {
+       fs.readFile('/home/pi/photo/now.jpg', function (err, data) {
+         if (err) throw err;
+         res.write(data);
+         res.end();
+       });
+     });
+   });
+
+   app.listen(8000, function() {
+     console.log("listening 8000");
+   });
+   
     serialPort.on("open", function (data) {
       console.log('open');
       console.log('data open: ' + data);
- 	  /*
-      request({
-		  uri: "http://192.168.43.26:8080/?id="+id+"&status=2&img_cctv="+img_cctv+"&helmet="+helmet,
-		  method: "GET"
-		  }, function(error, response, body) {
-			 console.log(response); 
-		});*/
+
 	  
       serialPort.on('data', function(data) {
-    	  console.log('data received: ' + data);
-    	  
-    	  
-    	 /* 
-
-
     	 
-    	  if( status.replace(/ /gi, "").replace(/\n/gi, "")  == "+panic" ) {
-    		  console.log('data received: ' + status);  
-    		  status = "";
-    	  } else {
-    		  if( status.indexOf(data) < 0 ) {
-    			  status += data;
-    			  console.log('data yet: ' + status); 
-    		  }
-    	  }
+    	  if( data == "+panic" ) {
+    		  console.log('data received: ' + data);
+    	 	  /*
+    	      request({
+    			  uri: "http://192.168.43.26:8080/?id="+id+"&status=2&img_cctv="+img_cctv+"&helmet="+helmet,
+    			  method: "GET"
+    			  }, function(error, response, body) {
+    				 console.log(response); 
+    			});*/
+       	  }
+    	  
+    	  if( data == "+fall" ) {
+    		  console.log('data received: ' + data);
+    	 	  /*
+    	      request({
+    			  uri: "http://192.168.43.26:8080/?id="+id+"&status=2&img_cctv="+img_cctv+"&helmet="+helmet,
+    			  method: "GET"
+    			  }, function(error, response, body) {
+    				 console.log(response); 
+    			});*/
+       	  }
 
-    	  if( status.replace(/ /gi, "").replace(/\n/gi, "") == "+fall" ) {
-    		  console.log('data received: ' + status);  
-    		  status = "";
-    	  } else {
-    		  if( status.indexOf(data) < 0 ) {
-    			  status += data;
-    			  console.log('data yet: ' + status); 
-    		  }
-    	  }
     	  
-    	  if( status.replace(/ /gi, "").replace(/\n/gi, "") == "+rise" ) {
-    		  console.log('data received: ' + status);  
-    		  status = "";
-    	  } else {
-    		  if( status.indexOf(data) < 0 ) {
-    			  status += data;
-    			  console.log('data yet: ' + status); 
-    		  }
-    	  }
+    	  if( data == "+wakeup" ) {
+    		  console.log('data received: ' + data);
+    	 	  /*
+    	      request({
+    			  uri: "http://192.168.43.26:8080/?id="+id+"&status=2&img_cctv="+img_cctv+"&helmet="+helmet,
+    			  method: "GET"
+    			  }, function(error, response, body) {
+    				 console.log(response); 
+    			});*/
+       	  }
     	  
-    	 */
+    	
     		  
       });
       
