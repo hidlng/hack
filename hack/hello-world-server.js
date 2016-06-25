5c50cdbdd83d83e3bf693b005781610f74f1cb06
kang <kang@kang-PC>
2016-06-25 15:46:20 +0900

    ddd

--- hack/hello-world-server.js
+++ hack/hello-world-server.js
@@ -1,7 +1,12 @@
-var http = require('http');
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '192.168.43.26');
+var serialport = require('serialport') ; 
+var SerialPort = require('serialport').SerialPort; 
 
-console.log('Server running at http://192.168.43.26:1337/');
+var serial = new SerialPort('/dev/ttyACM1', { 
+	baudrate : 9600,
+	parser: serialport.parsers.readline("\n")
+}) ; 
+
+
+serial.on('data', function(data) { 
+	console.log(data.toString()) ; 
+}) ; 
