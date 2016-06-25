cd8c78dbed275ef67aa49936abd9167755f40fcf
kang <kang@kang-PC>
2016-06-26 04:37:05 +0900

    123

--- hack/hello-world-server.js
+++ hack/hello-world-server.js
@@ -22,24 +22,20 @@
     serialPort.on("open", function (data) {
       console.log('open');
       console.log('data open: ' + data);
-	   var req = http.request(options, readJSONResponse);
-	   req.write('{"id":"1", "status":"2", "img_cctv" : "1.jpg", "helmet" : "2.jpg"}');
-	   req.end();
+ 	 request({
+		  uri: "http://52.79.138.81/saint/worker/update?id="+id+"&status=2&img_cctv="+img_cctv+"&helmet="+helmet,
+		  method: "GET"
+		  }, function(error, response, body) {
+			 console.log(response); 
+		});
 	  
       serialPort.on('data', function(data) {
     	  console.log('data received: ' + data);
     	  
     	  
     	 /* 
-    	  var req = http.request(options, readJSONResponse);
-    	  req.write('{"id":"1", "status":"2", "img_cctv" : "1.jpg", "helmet" : "2.jpg"}');
-    	  req.end();
-    	 request({
-			  uri: "http://52.79.138.81/saint/worker/update?id="+id+"&status="+status+"&img_cctv="+img_cctv+"&helmet="+helmet,
-			  method: "GET"
-			  }, function(error, response, body) {
-				 console.log(response); 
-			});
+
+
     	 
     	  if( status.replace(/ /gi, "").replace(/\n/gi, "")  == "+panic" ) {
     		  console.log('data received: ' + status);  
